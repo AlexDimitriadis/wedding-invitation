4a4f19b8cda68e6ccda6ffba8da8438bfe15ab1f
Alex Dimitriadis <dimalex95@proton.me>
2024-06-18 01:08:02 +0300

    Add spotify player

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import InvitationFront, {Card} from './components/Invitation';
 import {Locations,PortTabs} from './components/Locations';
 import Countdown from './components/Countdown';
 import Contact from './components/Contact';
+import { Spotify } from 'react-spotify-embed';
 
 function App() {
   return (
@@ -25,8 +26,9 @@ function App() {
         <br/>
         <h5 style={{fontWeight:'300'}} >Προαιρετική λίστα γάμου</h5>
         <h5 style={{margin:10, marginBottom:50}}>Τράπεζα Πειραιώς: GR94 0172 1480 0051 4811 2302 021
-
         </h5>
+        <Spotify style={{width:'80%', maxWidth:650, marginBottom:40}} wide link="https://open.spotify.com/track/6usohdchdzW9oML7VC4Uhk?si=658d75b211314a7d" />
+
 
       </header>
     </div>
